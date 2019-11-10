5b36f5023122e7f7c79e11ab7af655268cbbc12a
lakshay35@gmail.com <lakshay35@gmail.com>
2019-11-09 22:54:41 -0500

    Configured docker build

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API: 'http://localhost:8080',
+  API: 'http://localhost:4999',
   ALPHA_VANTAGE_KEY: '438CLJR2KP9I06PZ',
   ALPHA_VANTAGE_API: 'https://www.alphavantage.co/query?function='
 };
